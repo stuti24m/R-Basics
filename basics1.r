05b2f4677211adbc187e829a78bad5f70a1d4b31
Stuti Mittra <51860224+stuti24m@users.noreply.github.com>
2020-07-10 12:46:00 +0530

    Add files via upload

---
+++ basics1.r
@@ -0,0 +1,15 @@
+
+
+# variable 1
+var1 <- 2.5
+
+# variable2
+var2 <- 5
+
+var <- var2/var1
+
+# String
+
+greeting <- "Hello"
+name <- "Stuti"
+messgae <- paste(greeting,name)
---
+++ basics2.r
@@ -0,0 +1,16 @@
+# Logical 
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# &
+# | 
+# TRUE T
+# FALSE F
+5>4
+result <- 6>7
+result1 <-  10>2
+result | result1
