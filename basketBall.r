b45479d38a664f7fa7ca65fc83d78f58dec8f27b
Stuti Mittra <51860224+stuti24m@users.noreply.github.com>
2020-07-12 13:33:27 +0530

    Add files via upload

---
+++ basketBall.r
@@ -0,0 +1,43 @@
+
+
+
+Salary
+Points
+MinutesPlayed
+
+
+
+# matrix()
+
+?matrix
+Mydata <- 1:20
+A <- matrix(Mydata, 4,5)
+A
+A[2,3]
+
+B <- matrix(Mydata, 4,5, byrow = TRUE)
+B
+B[2,5]
+B[2,3]
+
+# rbind()
+
+r1 <- c("hey","how","why") 
+r2 <- c("whats","up","today")
+r3 <- c(1,2,3)
+C <- rbind(r1,r2,r3)
+C
+
+# cbind()
+
+c1 <- 1:3
+c2 <- -1:-3
+D <- cbind(c1,c2)
+D
+
+
+
+
+
+
+
---
+++ brackets[].r
@@ -0,0 +1,20 @@
+
+
+y <- c(1,2,3,4)  # combine
+x <- rep(1,10)   # replicate
+x
+y <- seq(1,15)   # sequence
+y
+
+w <- c("a","b","c","d","e")
+w
+
+w[1]
+w[2]
+w[-1]
+w[1:3]
+w[3:5]
+w[c(1,3,5)]
+
+
+lygometry
