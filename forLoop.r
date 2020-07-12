caac0eaf6b7caa61d70662e1ac4a3ed96c35585b
Stuti Mittra <51860224+stuti24m@users.noreply.github.com>
2020-07-12 13:32:05 +0530

    Add files via upload

---
+++ forLoop.r
@@ -0,0 +1,17 @@
+
+# for loop
+
+for(i in 1:5){
+  print("Hey There")
+  
+}
+
+# vector in a R is just a sequence of numbers unlike 
+#in other programming languages it is like an array
+
+for(i in 5:10){
+  print("Hey There")
+  
+}
+
+
